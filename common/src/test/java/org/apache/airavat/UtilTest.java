3626d24178d1c972ab3992461c5a4e5d66fbec61
Shaik Idris <psychidris@gmail.com>
2011-12-13 15:33:11 +0530

    1. Removed COORDINATOR static classes, 2. updated pom.xml to add dynamic jaxb generation 3. updated process.xml and process.xsd with new changes

---
+++ common/src/test/java/org/apache/airavat/UtilTest.java
@@ -0,0 +1,33 @@
+package org.apache.airavat;
+
+import java.io.InputStream;
+
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.validation.Schema;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.xml.sax.SAXException;
+
+public class UtilTest {
+
+	@Test
+	public void getSchema() throws SAXException {
+		Schema schema = Util.getSchema(UtilTest.class
+				.getResource("/coordinator.xsd"));
+		Assert.assertNotNull(schema);
+	}
+
+	@Test
+	public void getStreamFromString() {
+		InputStream stream = Util.getStreamFromString("<test>hi</test>");
+		Assert.assertNotNull(stream);
+	}
+
+	@Test
+	public void getUnmarshaller() throws JAXBException {
+		Unmarshaller unmarshaller = Util.getUnmarshaller(UtilTest.class);
+		Assert.assertNotNull(unmarshaller);
+	}
+}
