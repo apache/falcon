c28017e0abb9af0f91d6be0c1538109cfa113187
Shwetha GS <shwetha.gs@inmobi.com>
2012-01-31 12:28:23 +0530

    Fixed compilation error

--- webapp/src/main/java/org/apache/ivory/resource/EntityManager.java
+++ webapp/src/main/java/org/apache/ivory/resource/EntityManager.java
@@ -40,281 +40,265 @@ import org.apache.ivory.entity.v0.Entity;
 import org.apache.ivory.entity.v0.EntityType;
 import org.apache.ivory.workflow.EntityWorkflowManager;
 import org.apache.ivory.workflow.EntityWorkflowManagerFactory;
-import org.apache.ivory.workflow.engine.OozieWorkflowEngine;
 import org.apache.log4j.Logger;
 
 @Path("entities")
 public class EntityManager {
 
-	private static final Logger LOG = Logger.getLogger(EntityManager.class);
-	private static final Logger AUDIT = Logger.getLogger("AUDIT");
-	private enum WorkflowAction{SCHEDULE, DELETE, SUSPEND, RESUME};
+    private static final Logger LOG = Logger.getLogger(EntityManager.class);
+    private static final Logger AUDIT = Logger.getLogger("AUDIT");
 
-	/**
-	 * Submit a new entity. Entities can be of type feed, process or data end
-	 * points. Entity definitions are validated structurally against schema and
-	 * subsequently for other rules before they are admitted into the system
-	 * 
-	 * Entity name acts as the key and an entity once added, can't be added
-	 * again unless deleted.
-	 * 
-	 * @param type
-	 *            - feed, process or data end point
-	 * @return result of the operation
-	 */
-	@POST
-	@Path("submit/{type}")
-	@Consumes({ MediaType.TEXT_XML, MediaType.TEXT_PLAIN })
-	@Produces({ MediaType.TEXT_XML, MediaType.TEXT_PLAIN })
-	public APIResult submit(
-			@Context javax.servlet.http.HttpServletRequest request,
-			@PathParam("type") String type) {
+    private enum WorkflowAction {
+        SCHEDULE, DELETE, SUSPEND, RESUME
+    };
 
-		try {
-			EntityType entityType = EntityType.valueOf(type.toUpperCase());
-			EntityParser<?> entityParser = EntityParserFactory
-					.getParser(entityType);
-			InputStream xmlStream = request.getInputStream();
-			Entity entity = entityParser.parse(xmlStream);
-			ConfigurationStore configStore = ConfigurationStore.get();
-			Entity existingEntity = configStore.get(entityType, entity.getName());
-			if(existingEntity!=null){
-				LOG.error(entity.getName()+" already exists");
-				return new APIResult(APIResult.Status.FAILED, entity.getName()+" already exists");	
-			}
+    /**
+     * Submit a new entity. Entities can be of type feed, process or data end
+     * points. Entity definitions are validated structurally against schema and
+     * subsequently for other rules before they are admitted into the system
+     * 
+     * Entity name acts as the key and an entity once added, can't be added
+     * again unless deleted.
+     * 
+     * @param type
+     *            - feed, process or data end point
+     * @return result of the operation
+     */
+    @POST
+    @Path("submit/{type}")
+    @Consumes({ MediaType.TEXT_XML, MediaType.TEXT_PLAIN })
+    @Produces({ MediaType.TEXT_XML, MediaType.TEXT_PLAIN })
+    public APIResult submit(@Context javax.servlet.http.HttpServletRequest request, @PathParam("type") String type) {
 
-			configStore.publish(entityType, entity);
-			LOG.info("submit successful: " + entity.getName());
-		} catch (IvoryException e) {
-			LOG.error(e.getMessage());
-			return new APIResult(APIResult.Status.FAILED, e.getMessage());
-		} catch (IllegalArgumentException e) {
-			LOG.error(e.getMessage());
-			return new APIResult(APIResult.Status.FAILED, e.getMessage());
-		} catch (IOException e) {
-			LOG.error(e.getMessage());
-			return new APIResult(APIResult.Status.FAILED, e.getMessage());
-		}
-		return new APIResult(APIResult.Status.SUCCEEDED, "submit successful");
+        try {
+            EntityType entityType = EntityType.valueOf(type.toUpperCase());
+            EntityParser<?> entityParser = EntityParserFactory.getParser(entityType);
+            InputStream xmlStream = request.getInputStream();
+            Entity entity = entityParser.parse(xmlStream);
+            ConfigurationStore configStore = ConfigurationStore.get();
+            Entity existingEntity = configStore.get(entityType, entity.getName());
+            if (existingEntity != null) {
+                LOG.error(entity.getName() + " already exists");
+                return new APIResult(APIResult.Status.FAILED, entity.getName() + " already exists");
+            }
 
-	}
+            configStore.publish(entityType, entity);
+            LOG.info("submit successful: " + entity.getName());
+        } catch (IvoryException e) {
+            LOG.error(e.getMessage());
+            return new APIResult(APIResult.Status.FAILED, e.getMessage());
+        } catch (IllegalArgumentException e) {
+            LOG.error(e.getMessage());
+            return new APIResult(APIResult.Status.FAILED, e.getMessage());
+        } catch (IOException e) {
+            LOG.error(e.getMessage());
+            return new APIResult(APIResult.Status.FAILED, e.getMessage());
+        }
+        return new APIResult(APIResult.Status.SUCCEEDED, "submit successful");
 
-	/**
-	 * Post an entity XML with entity type. Validates the XML which can be
-	 * Process, Feed or Dataendpoint
-	 * 
-	 * @param type
-	 * @return APIResule -Succeeded or Failed
-	 */
-	@POST
-	@Path("validate/{type}")
-	@Consumes({ MediaType.TEXT_XML, MediaType.TEXT_PLAIN })
-	@Produces({ MediaType.TEXT_XML, MediaType.TEXT_PLAIN })
-	public APIResult validate(
-			@Context javax.servlet.http.HttpServletRequest request,
-			@PathParam("type") String type) {
+    }
 
-		try {
-			EntityType entityType = EntityType.valueOf(type.toUpperCase());
-			EntityParser<?> entityParser = EntityParserFactory
-					.getParser(entityType);
-			InputStream xmlStream = request.getInputStream();
-			entityParser.validateSchema(xmlStream);
-			LOG.info("Validate successful");
-		} catch (IOException e) {
-			LOG.error(e.getMessage());
-			return new APIResult(APIResult.Status.FAILED, e.getMessage());
-		} catch (IllegalArgumentException e) {
-			LOG.error(e.getMessage());
-			return new APIResult(APIResult.Status.FAILED, e.getMessage());
-		} catch (IvoryException e) {
-			LOG.error(e.getMessage());
-			return new APIResult(APIResult.Status.FAILED, e.getMessage());
-		}
+    /**
+     * Post an entity XML with entity type. Validates the XML which can be
+     * Process, Feed or Dataendpoint
+     * 
+     * @param type
+     * @return APIResule -Succeeded or Failed
+     */
+    @POST
+    @Path("validate/{type}")
+    @Consumes({ MediaType.TEXT_XML, MediaType.TEXT_PLAIN })
+    @Produces({ MediaType.TEXT_XML, MediaType.TEXT_PLAIN })
+    public APIResult validate(@Context javax.servlet.http.HttpServletRequest request, @PathParam("type") String type) {
 
-		return new APIResult(APIResult.Status.SUCCEEDED, "validate successful");
-	}
+        try {
+            EntityType entityType = EntityType.valueOf(type.toUpperCase());
+            EntityParser<?> entityParser = EntityParserFactory.getParser(entityType);
+            InputStream xmlStream = request.getInputStream();
+            entityParser.validateSchema(xmlStream);
+            LOG.info("Validate successful");
+        } catch (IOException e) {
+            LOG.error(e.getMessage());
+            return new APIResult(APIResult.Status.FAILED, e.getMessage());
+        } catch (IllegalArgumentException e) {
+            LOG.error(e.getMessage());
+            return new APIResult(APIResult.Status.FAILED, e.getMessage());
+        } catch (IvoryException e) {
+            LOG.error(e.getMessage());
+            return new APIResult(APIResult.Status.FAILED, e.getMessage());
+        }
 
-	/**
-	 * Schedules an submitted entity immediately
-	 * 
-	 * @param type
-	 * @param entity
-	 * @return APIResult
-	 */
-	@POST
-	@Path("schedule/{type}/{entity}")
-	@Produces({ MediaType.TEXT_XML, MediaType.TEXT_PLAIN })
-	public APIResult schedule(@PathParam("type") String type,
-			@PathParam("entity") String entity) {
-		return entityWorkflowAction(entity, type, WorkflowAction.SCHEDULE);	
-	}
+        return new APIResult(APIResult.Status.SUCCEEDED, "validate successful");
+    }
 
-	/**
-	 * Submits a new entity and schedules it immediately
-	 * 
-	 * @param type
-	 * @return
-	 */
-	@POST
-	@Path("submitAndSchedule/{type}")
-	@Consumes(MediaType.TEXT_PLAIN)
-	@Produces(MediaType.APPLICATION_JSON)
-	public APIResult submitAndSchedule(@PathParam("type") String type) {
-		return null;
-	}
+    /**
+     * Schedules an submitted entity immediately
+     * 
+     * @param type
+     * @param entity
+     * @return APIResult
+     */
+    @POST
+    @Path("schedule/{type}/{entity}")
+    @Produces({ MediaType.TEXT_XML, MediaType.TEXT_PLAIN })
+    public APIResult schedule(@PathParam("type") String type, @PathParam("entity") String entity) {
+        return entityWorkflowAction(entity, type, WorkflowAction.SCHEDULE);
+    }
 
-	/**
-	 * Deletes a scheduled entity, a deleted entity is removed completely from
-	 * execution pool.
-	 * 
-	 * @param type
-	 * @param entity
-	 * @return APIResult
-	 */
-	@DELETE
-	@Path("delete/{type}/{entity}")
-	@Produces({ MediaType.TEXT_XML, MediaType.TEXT_PLAIN })
-	public APIResult delete(@PathParam("type") String type,
-			@PathParam("entity") String entity) {
+    /**
+     * Submits a new entity and schedules it immediately
+     * 
+     * @param type
+     * @return
+     */
+    @POST
+    @Path("submitAndSchedule/{type}")
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public APIResult submitAndSchedule(@PathParam("type") String type) {
+        return null;
+    }
 
-		APIResult apiResult = entityWorkflowAction(entity, type, WorkflowAction.DELETE);
+    /**
+     * Deletes a scheduled entity, a deleted entity is removed completely from
+     * execution pool.
+     * 
+     * @param type
+     * @param entity
+     * @return APIResult
+     */
+    @DELETE
+    @Path("delete/{type}/{entity}")
+    @Produces({ MediaType.TEXT_XML, MediaType.TEXT_PLAIN })
+    public APIResult delete(@PathParam("type") String type, @PathParam("entity") String entity) {
 
-		if(apiResult.getStatus().equals(APIResult.Status.SUCCEEDED)){
-			ConfigurationStore configStore = ConfigurationStore.get();		
-			try {
-				configStore.remove(EntityType.valueOf(type.toUpperCase()), entity);
-			} catch (StoreAccessException e) {
-				return new APIResult(APIResult.Status.FAILED, e.getMessage());
-			}
-		}
+        APIResult apiResult = entityWorkflowAction(entity, type, WorkflowAction.DELETE);
 
-		return apiResult;
-	}
+        if (apiResult.getStatus().equals(APIResult.Status.SUCCEEDED)) {
+            ConfigurationStore configStore = ConfigurationStore.get();
+            try {
+                configStore.remove(EntityType.valueOf(type.toUpperCase()), entity);
+            } catch (StoreAccessException e) {
+                return new APIResult(APIResult.Status.FAILED, e.getMessage());
+            }
+        }
 
-	/**
-	 * Suspends a running entity
-	 * 
-	 * @param type
-	 * @param entity
-	 * @return APIResult
-	 */
-	@POST
-	@Path("suspend/{type}/{entity}")
-	@Produces({ MediaType.TEXT_XML, MediaType.TEXT_PLAIN })
-	public APIResult suspend(@PathParam("type") String type,
-			@PathParam("entity") String entity) {
-		return entityWorkflowAction(entity, type, WorkflowAction.SUSPEND);	
-	}
+        return apiResult;
+    }
 
-	/**
-	 * Resumes a suspended entity
-	 * 
-	 * @param type
-	 * @param entity
-	 * @return APIResult
-	 */
-	@POST
-	@Path("resume/{type}/{entity}")
-	@Produces({ MediaType.TEXT_XML, MediaType.TEXT_PLAIN })
-	public APIResult resume(@PathParam("type") String type,
-			@PathParam("entity") String entity) {
-		
-		return entityWorkflowAction(entity, type, WorkflowAction.RESUME);	
-	}
+    /**
+     * Suspends a running entity
+     * 
+     * @param type
+     * @param entity
+     * @return APIResult
+     */
+    @POST
+    @Path("suspend/{type}/{entity}")
+    @Produces({ MediaType.TEXT_XML, MediaType.TEXT_PLAIN })
+    public APIResult suspend(@PathParam("type") String type, @PathParam("entity") String entity) {
+        return entityWorkflowAction(entity, type, WorkflowAction.SUSPEND);
+    }
 
-	/**
-	 * Returns the status of requested entity.
-	 * 
-	 * @param type
-	 * @param entity
-	 * @return String
-	 */
-	@GET
-	@Path("status/{type}/{entity}")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String getStatus(@PathParam("type") String type,
-			@PathParam("entity") String entity) {
-		return "hello Hi ...\n";
-	}
+    /**
+     * Resumes a suspended entity
+     * 
+     * @param type
+     * @param entity
+     * @return APIResult
+     */
+    @POST
+    @Path("resume/{type}/{entity}")
+    @Produces({ MediaType.TEXT_XML, MediaType.TEXT_PLAIN })
+    public APIResult resume(@PathParam("type") String type, @PathParam("entity") String entity) {
 
-	/**
-	 * Returns the entity definition as an XML based on name
-	 * 
-	 * @param type
-	 * @param entityName
-	 * @return String
-	 */
-	@GET
-	@Path("definition/{type}/{entity}")
-	@Produces({ MediaType.TEXT_XML, MediaType.TEXT_PLAIN })
-	public String getEntityDefinition(@PathParam("type") String type,
-			@PathParam("entity") String entityName) {
-		try {
-			EntityType entityType = EntityType.valueOf(type.toUpperCase());
-			ConfigurationStore configStore = ConfigurationStore.get();
-			Entity entity= configStore.get(entityType, entityName);
-			if(entity==null){
-				LOG.error(entityName+" does not exists");
-				return new APIResult(APIResult.Status.FAILED, entityName+" does not exists").toString();	
-			}
-			LOG.info("Returned entity: " + entity);
-			return entity.toString();
-		} catch (IllegalArgumentException e) {
-			LOG.error(e.getMessage());
-			return new APIResult(APIResult.Status.FAILED, e.getMessage()).toString();
-		} catch (StoreAccessException e) {
-			LOG.error(e.getMessage());
-			return new APIResult(APIResult.Status.FAILED, e.getMessage()).toString();
-		}
-	}
+        return entityWorkflowAction(entity, type, WorkflowAction.RESUME);
+    }
 
-	private APIResult entityWorkflowAction(String entity, String type,
-			WorkflowAction workflowAction) {
-		try {
-			EntityType entityType = EntityType.valueOf(type.toUpperCase());
-			ConfigurationStore configStore = ConfigurationStore.get();
-			Entity entityObj = configStore.get(entityType, entity);
-			// Currently only process/coordinator is removed
-			if (entityObj == null) {
-				return new APIResult(APIResult.Status.FAILED, "Entity: "
-						+ entity + " does not exists");
-			}
-			//TODO currently these operation are supported only for PROCESS
-			if (entityType.equals(EntityType.PROCESS)) {
-				EntityWorkflowManager<Entity> entityWorkflowManager = EntityWorkflowManagerFactory
-						.getWorkflowManager(entityObj);
-				entityWorkflowManager
-						.setWorkflowEngine(new OozieWorkflowEngine());
+    /**
+     * Returns the status of requested entity.
+     * 
+     * @param type
+     * @param entity
+     * @return String
+     */
+    @GET
+    @Path("status/{type}/{entity}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getStatus(@PathParam("type") String type, @PathParam("entity") String entity) {
+        return "hello Hi ...\n";
+    }
 
-				switch (workflowAction) {
-				case SCHEDULE:
-					entityWorkflowManager.schedule(entityObj);
-					break;
-				case DELETE:
-					entityWorkflowManager.delete(entityObj);
-					break;
-				case SUSPEND:
-					entityWorkflowManager.suspend(entityObj);
-					break;
-				case RESUME:
-					entityWorkflowManager.resume(entityObj);
-					break;
-				}
-			}
+    /**
+     * Returns the entity definition as an XML based on name
+     * 
+     * @param type
+     * @param entityName
+     * @return String
+     */
+    @GET
+    @Path("definition/{type}/{entity}")
+    @Produces({ MediaType.TEXT_XML, MediaType.TEXT_PLAIN })
+    public String getEntityDefinition(@PathParam("type") String type, @PathParam("entity") String entityName) {
+        try {
+            EntityType entityType = EntityType.valueOf(type.toUpperCase());
+            ConfigurationStore configStore = ConfigurationStore.get();
+            Entity entity = configStore.get(entityType, entityName);
+            if (entity == null) {
+                LOG.error(entityName + " does not exists");
+                return new APIResult(APIResult.Status.FAILED, entityName + " does not exists").toString();
+            }
+            LOG.info("Returned entity: " + entity);
+            return entity.toString();
+        } catch (IllegalArgumentException e) {
+            LOG.error(e.getMessage());
+            return new APIResult(APIResult.Status.FAILED, e.getMessage()).toString();
+        } catch (StoreAccessException e) {
+            LOG.error(e.getMessage());
+            return new APIResult(APIResult.Status.FAILED, e.getMessage()).toString();
+        }
+    }
 
-		} catch (IllegalArgumentException e) {
-			LOG.error(e.getMessage());
-			return new APIResult(APIResult.Status.FAILED, e.getMessage());
-		} catch (StoreAccessException e) {
-			LOG.error(e.getMessage());
-			return new APIResult(APIResult.Status.FAILED, e.getMessage());
-		} catch (IvoryException e) {
-			LOG.error(e.getMessage());
-			return new APIResult(APIResult.Status.FAILED, e.getMessage());
-		}
-		LOG.info(workflowAction.name().toLowerCase()+" successful");
-		return new APIResult(APIResult.Status.SUCCEEDED,
-				workflowAction.name().toLowerCase()+" successful");
-	}
+    private APIResult entityWorkflowAction(String entity, String type, WorkflowAction workflowAction) {
+        try {
+            EntityType entityType = EntityType.valueOf(type.toUpperCase());
+            ConfigurationStore configStore = ConfigurationStore.get();
+            Entity entityObj = configStore.get(entityType, entity);
+            // Currently only process/coordinator is removed
+            if (entityObj == null) {
+                return new APIResult(APIResult.Status.FAILED, "Entity: " + entity + " does not exists");
+            }
+            // TODO currently these operation are supported only for PROCESS
+            if (entityType.equals(EntityType.PROCESS)) {
+                EntityWorkflowManager<Entity> entityWorkflowManager = EntityWorkflowManagerFactory.getWorkflowManager(entityObj);
+
+                switch (workflowAction) {
+                    case SCHEDULE:
+                        entityWorkflowManager.schedule(entityObj);
+                        break;
+                    case DELETE:
+                        entityWorkflowManager.delete(entityObj);
+                        break;
+                    case SUSPEND:
+                        entityWorkflowManager.suspend(entityObj);
+                        break;
+                    case RESUME:
+                        entityWorkflowManager.resume(entityObj);
+                        break;
+                }
+            }
+
+        } catch (IllegalArgumentException e) {
+            LOG.error(e.getMessage());
+            return new APIResult(APIResult.Status.FAILED, e.getMessage());
+        } catch (StoreAccessException e) {
+            LOG.error(e.getMessage());
+            return new APIResult(APIResult.Status.FAILED, e.getMessage());
+        } catch (IvoryException e) {
+            LOG.error(e.getMessage());
+            return new APIResult(APIResult.Status.FAILED, e.getMessage());
+        }
+        LOG.info(workflowAction.name().toLowerCase() + " successful");
+        return new APIResult(APIResult.Status.SUCCEEDED, workflowAction.name().toLowerCase() + " successful");
+    }
 }
