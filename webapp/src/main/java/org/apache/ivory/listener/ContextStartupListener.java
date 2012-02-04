dd10a2c3f24e940ad1154c6d9ab6b9af081a360a
Srikanth Sundarrajan <sriksun@hotmail.com>
2012-02-04 11:09:10 +0530

    Build Info

--- webapp/src/main/java/org/apache/ivory/listener/ContextStartupListener.java
+++ webapp/src/main/java/org/apache/ivory/listener/ContextStartupListener.java
@@ -18,6 +18,9 @@
 
 package org.apache.ivory.listener;
 
+import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
 import org.apache.ivory.util.RuntimeProperties;
 import org.apache.ivory.util.StartupProperties;
 import org.apache.log4j.Logger;
@@ -27,18 +30,50 @@ import javax.servlet.ServletContextListener;
 
 public class ContextStartupListener implements ServletContextListener {
 
-  private static Logger LOG = Logger.getLogger(ContextStartupListener.class);
+    private static Logger LOG = Logger.getLogger(ContextStartupListener.class);
 
-  @Override
-  public void contextInitialized(ServletContextEvent sce) {
-    //Initialize Startup and runtime properties instance for use
-    LOG.info("Initializing startup properties ...");
-    StartupProperties.get();
-    LOG.info("Initializing runtime properties ...");
-    RuntimeProperties.get();
-  }
+    private static final String BUILD_PROPERTIES = "ivory-buildinfo.properties";
 
-  @Override
-  public void contextDestroyed(ServletContextEvent sce) {
-  }
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        //Initialize Startup and runtime properties instance for use
+        LOG.info("Initializing startup properties ...");
+        StartupProperties.get();
+
+        showStartupInfo();
+
+        LOG.info("Initializing runtime properties ...");
+        RuntimeProperties.get();
+    }
+
+    private void showStartupInfo() {
+        StringBuilder buffer = new StringBuilder();
+        buffer.append("\n############################################");
+        buffer.append("############################################");
+        buffer.append("\n                               Ivory Server (STARTUP)");
+        buffer.append("\n");
+        Properties buildProperties = new Properties();
+        try {
+            buildProperties.load(getClass().getClassLoader().
+                        getResourceAsStream(BUILD_PROPERTIES));
+            for (Map.Entry entry : buildProperties.entrySet()) {
+                buffer.append('\n').append('\t').append(entry.getKey()).
+                        append(":\t").append(entry.getValue());
+            }
+        } catch (Throwable e) {
+            buffer.append("*** Unable to get build info ***");
+        }
+        buffer.append("\n############################################");
+        buffer.append("############################################");
+        LOG.info(buffer);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        StringBuilder buffer = new StringBuilder();
+        buffer.append("\n############################################");
+        buffer.append("\n         Ivory Server (SHUTDOWN)            ");
+        buffer.append("\n############################################");
+        LOG.info(buffer);
+    }
 }
