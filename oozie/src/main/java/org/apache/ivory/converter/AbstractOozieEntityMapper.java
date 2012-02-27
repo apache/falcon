0b82645de5e22c90c5901135a6273633e2ada621
Srikanth Sundarrajan <sriksun@hotmail.com>
2012-02-27 13:23:15 +0530

    Refactored OozieProcessMapper

---
+++ oozie/src/main/java/org/apache/ivory/converter/AbstractOozieEntityMapper.java
@@ -0,0 +1,9 @@
+package org.apache.ivory.converter;
+
+import org.apache.log4j.Logger;
+
+public class AbstractOozieEntityMapper {
+
+    private static Logger LOG = Logger.getLogger(AbstractOozieEntityMapper.class);
+
+}
