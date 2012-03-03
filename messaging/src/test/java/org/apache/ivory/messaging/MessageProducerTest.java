996afbd9a9ba669d1b5a88b28c524beaf86d91c8
Shaik Idris <psychidris@gmail.com>
2012-03-03 22:50:00 +0530

    Added consumer logic for catching assertions

--- messaging/src/test/java/org/apache/ivory/messaging/MessageProducerTest.java
+++ messaging/src/test/java/org/apache/ivory/messaging/MessageProducerTest.java
@@ -18,30 +18,85 @@
 
 package org.apache.ivory.messaging;
 
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class MessageProducerTest {
 
 	private ProcessMessage msgArgs;
+	private static final String TEST_CONN_URL = "vm://localhost?broker.useJmx=false&broker.persistent=true";
+	private static final String TOPIC_NAME = "Ivory.process1.click-logs";
+	private volatile AssertionError error;
 
 	@BeforeClass
 	public void setArgs() {
 		this.msgArgs = new ProcessMessage();
-		this.msgArgs.setProcessTopicName("Ivory.process1.click-logs");
+		this.msgArgs.setProcessTopicName(TOPIC_NAME);
 		this.msgArgs.setFeedName("click-logs,");
 		this.msgArgs.setFeedInstancePath("/click-logs/10/05/05/00/20,");
 		this.msgArgs.setWorkflowId("workflow-01-00");
 		this.msgArgs.setRunId("1");
 		this.msgArgs.setNominalTime("2011-01-01");
 		this.msgArgs.setTimeStamp("2012-01-01");
-		this.msgArgs.setBrokerUrl("tcp://localhost:61616?daemon=true");
+		this.msgArgs.setBrokerUrl(TEST_CONN_URL);
 	}
 
 	@Test
-	public void testProcessMessageCreator() {
+	public void testProcessMessageCreator() throws JMSException,
+			InterruptedException {
+
+		Thread t = new Thread() {
+			@Override
+			public void run() {
+				try {
+					consumer();
+				} catch (AssertionError e) {
+					error=e;
+				} catch (JMSException ignore) {
+					
+				}
+			}
+		};
+		t.start();
+		Thread.sleep(1500);
 		MessageProducer.main(ArgumentsResolver
 				.resolveToStringArray(new ProcessMessage[] { this.msgArgs }));
+		if(error!=null){
+			throw error;
+		}
 	}
 
+	private void consumer() throws JMSException {
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
+				TEST_CONN_URL);
+		Connection connection = connectionFactory.createConnection();
+		connection.start();
+
+		Session session = connection.createSession(false,
+				Session.AUTO_ACKNOWLEDGE);
+		Destination destination = session.createTopic(TOPIC_NAME);
+		MessageConsumer consumer = session.createConsumer(destination);
+		Message m = consumer.receive();
+
+		if (m != null) {
+			TextMessage textMessage = (TextMessage) m;
+			String[] items = textMessage.getText().split(",");
+			Assert.assertEquals(items[0], TOPIC_NAME);
+			Assert.assertEquals(items[1], "click-logs");
+			Assert.assertEquals(items[2], "/click-logs/10/05/05/00/20");
+			Assert.assertEquals(items[3], "workflow-01-00");
+		}
+		connection.close();
+	}
 }
