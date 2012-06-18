f169344b97cb253c8122d3025218cf0af6a7e3bd
shaik.idris <psychidris@gmail.com>
2012-06-18 19:39:25 +0530

    UTs for Ivory API's, all enabled

--- client/src/main/java/org/apache/ivory/client/IvoryClient.java
+++ client/src/main/java/org/apache/ivory/client/IvoryClient.java
@@ -83,13 +83,13 @@ public class IvoryClient {
 		if (input != null) {
 			prop.load(input);
 			readTimeout = prop.containsKey("ivory.read.timeout") ? Integer
-					.parseInt(prop.getProperty("ivory.read.timeout")) : 60000;
+					.parseInt(prop.getProperty("ivory.read.timeout")) : 180000;
 			connectTimeout = prop.containsKey("ivory.connect.timeout") ? Integer
 					.parseInt(prop.getProperty("ivory.connect.timeout"))
-					: 60000;
+					: 180000;
 		} else{
-			readTimeout = 60000;
-			connectTimeout = 60000;
+			readTimeout = 180000;
+			connectTimeout = 180000;
 		}
 		client.setConnectTimeout(connectTimeout);
 		client.setReadTimeout(readTimeout);
@@ -108,7 +108,7 @@ public class IvoryClient {
 				"api/entities/suspend/", HttpMethod.POST, MediaType.TEXT_XML), RESUME(
 				"api/entities/resume/", HttpMethod.POST, MediaType.TEXT_XML), DELETE(
 				"api/entities/delete/", HttpMethod.DELETE, MediaType.TEXT_XML), STATUS(
-				"api/entities/status/", HttpMethod.GET, MediaType.TEXT_PLAIN), DEFINITION(
+				"api/entities/status/", HttpMethod.GET, MediaType.TEXT_XML), DEFINITION(
 				"api/entities/definition/", HttpMethod.GET, MediaType.TEXT_XML), LIST(
 				"api/entities/list/", HttpMethod.GET, MediaType.TEXT_XML), DEPENDENCY(
 				"api/entities/dependencies/", HttpMethod.GET,
