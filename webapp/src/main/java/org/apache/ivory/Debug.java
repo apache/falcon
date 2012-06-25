4bc8110a2bf8cd74a3074d30cff93dca7f21852e
Srikanth Sundarrajan <Sriksun@hotmail.com>
2012-06-25 12:02:40 +0530

    Handling delays in oozie actions

--- webapp/src/main/java/org/apache/ivory/Debug.java
+++ webapp/src/main/java/org/apache/ivory/Debug.java
@@ -5,18 +5,18 @@ import org.apache.ivory.entity.EntityUtil;
 import org.apache.ivory.entity.store.ConfigurationStore;
 import org.apache.ivory.entity.v0.Entity;
 import org.apache.ivory.entity.v0.EntityType;
+import org.apache.ivory.resource.AbstractInstanceManager;
 import org.apache.ivory.resource.InstancesResult;
+import org.apache.ivory.resource.channel.NullServletRequest;
+import org.apache.ivory.resource.proxy.InstanceManagerProxy;
+import org.apache.ivory.security.CurrentUser;
 import org.apache.ivory.service.Services;
-import org.apache.ivory.workflow.engine.OozieWorkflowEngine;
 import org.apache.log4j.Logger;
 
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.ByteArrayInputStream;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Properties;
 
 public class Debug {
     private static final Logger LOG = Logger.getLogger(Debug.class);
@@ -30,7 +30,7 @@ public class Debug {
 
         Services.get().register(ConfigurationStore.get());
         ConfigurationStore.get().init();
-
+        CurrentUser.authenticate("testuser");
         IvoryClient client = new IvoryClient(ivoryUrl);
         String[] deps = client.getDependency(type, entity).split("\n");
         for (String line : deps) {
@@ -45,11 +45,15 @@ public class Debug {
 
         Entity obj = EntityUtil.getEntity(type, entity);
 
-        OozieWorkflowEngine engine = new OozieWorkflowEngine();
-        Date start = formatter.parse("2010-01-02 01:15 UTC");
-        Date end = formatter.parse("2010-01-02 01:20 UTC");
-        InstancesResult status = engine.killInstances(obj, start, end, new Properties());
-        System.out.println(Arrays.toString(status.getInstances()));
+//        OozieWorkflowEngine engine = new OozieWorkflowEngine();
+//        Date start = formatter.parse("2010-01-02 01:05 UTC");
+//        Date end = formatter.parse("2010-01-02 01:21 UTC");
+//        InstancesResult status = engine.suspendInstances(obj, start, end, new Properties());
+//        System.out.println(Arrays.toString(status.getInstances()));
+        AbstractInstanceManager manager = new InstanceManagerProxy();
+        InstancesResult result = manager.suspendInstance(new NullServletRequest(), type, entity,
+                "2010-01-02T01:05Z", "2010-01-02T01:21Z", "*");
+        System.out.println(result);
     }
 
     private static void store(EntityType eType, String xml) throws JAXBException, IvoryException {
