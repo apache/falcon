de085733c0c14bf363078aa192ddb212b9ce27c6
Praveen Adlakha <adlakha.praveen@gmail.com>
2016-02-11 17:15:06 +0530

    FALCON-1811 Status API does not honour start option

--- prism/src/main/java/org/apache/falcon/resource/AbstractInstanceManager.java
+++ prism/src/main/java/org/apache/falcon/resource/AbstractInstanceManager.java
@@ -485,8 +485,17 @@ public abstract class AbstractInstanceManager extends AbstractEntityManager {
                             : end2.compareTo(end1);
                 }
             });
-        }//Default : no sort
-
+        }
+        Collections.sort(instanceSet, new Comparator<Instance>() {
+            @Override
+            public int compare(Instance i1, Instance i2) {
+                Date start1 = (i1.getStartTime() == null) ? new Date(0) : i1.getStartTime();
+                Date start2 = (i2.getStartTime() == null) ? new Date(0) : i2.getStartTime();
+                return (order.equalsIgnoreCase("asc")) ? start1.compareTo(start2)
+                        : start2.compareTo(start1);
+            }
+        });
+        //Default : startTime
         return instanceSet;
     }
 
