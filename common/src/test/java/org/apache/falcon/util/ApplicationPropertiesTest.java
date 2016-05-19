a9119c6963b7862f5c784a6ffff8d3b1551ca6b5
Peeyush <peeyushb@apache.org>
2016-05-19 18:03:23 +0530

    FALCON-1973 Falcon build failure due to checkstyle issue
    Please review the following pull request in which Falcon build failure is happening with checkstyle issue from the FALCON-1964 patch.
    
    Author: peeyush b <pbishnoi@hortonworks.com>
    
    Reviewers: Pavan <pavan.kolamuri@gmail.com>
    
    Closes #149 from peeyushb/FALCON-1973

--- common/src/test/java/org/apache/falcon/util/ApplicationPropertiesTest.java
+++ common/src/test/java/org/apache/falcon/util/ApplicationPropertiesTest.java
@@ -40,14 +40,14 @@ public class ApplicationPropertiesTest {
     private static final String PROPERTY_2 = "property-key-2";
     private static final String JKS_FILE_NAME = "credentials.jks";
 
-    private static final File credDir = new File(".");
+    private static final File CRED_DIR = new File(".");
 
     @AfterClass
     public void tearDown() throws Exception {
         // delete temporary jks files
-        File file = new File(credDir, JKS_FILE_NAME);
+        File file = new File(CRED_DIR, JKS_FILE_NAME);
         file.delete();
-        file = new File(credDir, "." + JKS_FILE_NAME + ".crc");
+        file = new File(CRED_DIR, "." + JKS_FILE_NAME + ".crc");
         file.delete();
     }
 
@@ -57,12 +57,12 @@ public class ApplicationPropertiesTest {
         Assert.assertTrue(CredentialProviderHelper.isProviderAvailable());
 
         // clean credential provider store
-        File file = new File(credDir, JKS_FILE_NAME);
+        File file = new File(CRED_DIR, JKS_FILE_NAME);
         file.delete();
 
         // add alias to hadoop credential provider
         Configuration conf = new Configuration();
-        String providerPath = "jceks://file/" + credDir.getAbsolutePath() + "/" + JKS_FILE_NAME;
+        String providerPath = "jceks://file/" + CRED_DIR.getAbsolutePath() + "/" + JKS_FILE_NAME;
         conf.set(CredentialProviderHelper.CREDENTIAL_PROVIDER_PATH, providerPath);
         CredentialProviderHelper.createCredentialEntry(conf, ALIAS_1, PASSWORD_1);
         CredentialProviderHelper.createCredentialEntry(conf, ALIAS_2, PASSWORD_2);
