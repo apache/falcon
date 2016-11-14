4b5fa41f112779a966540576bc8ffafa33287084
sandeep <sandysmdl@gmail.com>
2016-11-14 10:46:03 +0530

    FALCON-753 , FALCON-754. Correcting the authentication check in SLA and authenticating right user in Entity sla.

--- prism/src/main/java/org/apache/falcon/service/EntitySLAMonitoringService.java
+++ prism/src/main/java/org/apache/falcon/service/EntitySLAMonitoringService.java
@@ -28,7 +28,7 @@ import java.util.Set;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.falcon.FalconException;
 import org.apache.falcon.LifeCycle;
 import org.apache.falcon.Pair;
