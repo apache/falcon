74e7383c64ab5d1caa6173daa56cebe5520840a8
Praveen Adlakha <adlakha.praveen@gmail.com>
2016-11-18 14:04:36 +0530

    checkstyle issue fixed

--- extensions/src/main/java/org/apache/falcon/extensions/store/ExtensionStore.java
+++ extensions/src/main/java/org/apache/falcon/extensions/store/ExtensionStore.java
@@ -48,7 +48,6 @@ import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import scala.tools.nsc.interactive.RangePositions;
 
 /**
  * Store for Falcon extensions.
