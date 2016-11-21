9e25ede1f871ab86db50d4ff4833b0f85901f03d
sandeep <sandysmdl@gmail.com>
2016-11-21 20:00:58 +0530

    FALCON-2187 Refactoring to move entity xsd and jxb bindings to a new nmodule
    Author: sandeep <sandysmdl@gmail.com>
    
    Reviewers: @pallavi-rao
    
    Closes #303 from sandeepSamudrala/FALCON-2187 and squashes the following commits:
    
    eed6ff1 [sandeep] FALCON-2187 dummy commit
    579d39a [sandeep] FALCON-2187 moving resource folder too from client to common-types as its creating a cyclic dependency from client to extensions to common to client
    c4815b7 [sandeep] FALCON-2187 litlle formatting in Tag class
    fb8aed9 [sandeep] Merge branch 'master' of https://github.com/apache/falcon into FALCON-2187
    846f566 [sandeep] Merge branch 'master' of https://github.com/apache/falcon into FALCON-2187
    0b3aff8 [sandeep] FALCON-2187 Moving ExtensionType to common-types
    7534497 [sandeep] FALCON-2187 Refactoring to move entity xsd and jxb bindings to a new nmodule
    3a7be21 [sandeep] Merge branch 'master' of https://github.com/apache/falcon into FALCON-2187
    2baea85 [sandeep] Merge branch 'master' of https://github.com/apache/falcon into FALCON-2187
    8aacd75 [sandeep] FALCON-2183 Incorporated review comments
    f3d7268 [sandeep] FALCON-2183 Incorporated review comments
    11e7b3f [sandeep] FALCON-2183 Extension Builder changes to support new user extensions
    250cc46 [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    d0393e9 [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    a178805 [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    d6dc8bf [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    1bb8d3c [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    c065566 [sandeep] reverting last line changes made
    1a4dcd2 [sandeep] rebased and resolved the conflicts from master
    271318b [sandeep] FALCON-2097. Adding UT to the new method for getting next instance time with Delay.
    a94d4fe [sandeep] rebasing from master
    9e68a57 [sandeep] FALCON-298. Feed update with replication delay creates holes

--- common-types/src/main/java/org/apache/falcon/Tag.java
+++ common-types/src/main/java/org/apache/falcon/Tag.java
@@ -24,12 +24,15 @@ import org.apache.falcon.entity.v0.EntityType;
  * Tag to include in the entity type.
  */
 public enum Tag {
-    DEFAULT(EntityType.PROCESS), RETENTION(EntityType.FEED), REPLICATION(EntityType.FEED),
-        IMPORT(EntityType.FEED), EXPORT(EntityType.FEED);
+    DEFAULT(EntityType.PROCESS),
+    RETENTION(EntityType.FEED),
+    REPLICATION(EntityType.FEED),
+    IMPORT(EntityType.FEED),
+    EXPORT(EntityType.FEED);
 
     private final EntityType entityType;
 
-    private Tag(EntityType entityType) {
+    Tag(EntityType entityType) {
         this.entityType = entityType;
     }
 
