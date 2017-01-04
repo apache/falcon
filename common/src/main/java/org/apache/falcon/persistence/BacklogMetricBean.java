280a079134690ec7801630f0e674ef96975761a5
sandeep <sandysmdl@gmail.com>
2017-01-04 10:01:15 +0530

    FALCON-2229 Removed cluster from the named query that deletes all the instances irrespective of the cluster

--- common/src/main/java/org/apache/falcon/persistence/BacklogMetricBean.java
+++ common/src/main/java/org/apache/falcon/persistence/BacklogMetricBean.java
@@ -39,7 +39,7 @@ import java.util.Date;
 @NamedQueries({
         @NamedQuery(name = PersistenceConstants.GET_ALL_BACKLOG_INSTANCES, query = "select  OBJECT(a) from BacklogMetricBean a "),
         @NamedQuery(name = PersistenceConstants.DELETE_BACKLOG_METRIC_INSTANCE, query = "delete from BacklogMetricBean a where a.entityName = :entityName and a.clusterName = :clusterName and a.nominalTime = :nominalTime and a.entityType = :entityType"),
-        @NamedQuery(name = PersistenceConstants.DELETE_ALL_BACKLOG_ENTITY_INSTANCES, query = "delete from BacklogMetricBean a where a.entityName = :entityName and a.clusterName = :clusterName and a.entityType = :entityType")
+        @NamedQuery(name = PersistenceConstants.DELETE_ALL_BACKLOG_ENTITY_INSTANCES, query = "delete from BacklogMetricBean a where a.entityName = :entityName and a.entityType = :entityType")
 })
 //RESUME CHECKSTYLE CHECK  LineLengthCheck
 
