4db747682281a6e1cb55a6d4d812da6e5199e8c6
Pracheer Agarwal <pracheer.agarwal@inmobi.com>
2017-02-08 14:20:44 +0530

    FALCON-2275 making instances API unsupported for extensions
    Author: Pracheer Agarwal <pracheer.agarwal@inmobi.com>
    Author: Pracheer Agarwal <pracheeragarwal@gmail.com>
    Author: Pracheer Agarwal <pr@im2216-x0.corp.inmobi.com>
    
    Reviewers: @sandeepSamudrala,@pallavi-rao
    
    Closes #359 from PracheerAgarwal/FALCON-2275 and squashes the following commits:
    
    3f83942 [Pracheer Agarwal] review comments changes
    90aef9a [Pracheer Agarwal] FALCON-2275 making instances API unsupported for extensions
    af6e719 [Pracheer Agarwal] FALCON-2275 making instances API unsupported for extensions
    ba60452 [Pracheer Agarwal] Merge branch 'master' of https://github.com/apache/falcon
    ed65aa0 [Pracheer Agarwal] Merge branch 'master' of https://github.com/apache/falcon
    9ff05df [Pracheer Agarwal] Merge branch 'master' of https://github.com/apache/falcon
    9c2f0a5 [Pracheer Agarwal] Merge branch 'master' of https://github.com/apache/falcon
    9cd8c17 [Pracheer Agarwal] Merge branch 'master' of https://github.com/apache/falcon
    778c579 [Pracheer Agarwal] Merge branch 'master' of https://github.com/PracheerAgarwal/falcon
    e39808d [Pracheer Agarwal] Merge branch 'master' of https://github.com/apache/falcon
    a932633 [Pracheer Agarwal] Merge branch 'master' of https://github.com/apache/falcon
    fda3b28 [Pracheer Agarwal] Merge branch 'master' of https://github.com/apache/falcon
    a93d71a [Pracheer Agarwal] Merge branch 'master' of https://github.com/PracheerAgarwal/falcon
    e3728d5 [Pracheer Agarwal] Merge branch 'master' of https://github.com/apache/falcon
    066c8e2 [Pracheer Agarwal] Merge branch 'master' of https://github.com/apache/falcon
    b20f044 [Pracheer Agarwal] Merge branch 'master' of https://github.com/apache/falcon
    7f572a1 [Pracheer Agarwal] Merge branch 'master' of https://github.com/apache/falcon
    46042fd [Pracheer Agarwal] Merge branch 'master' of https://github.com/PracheerAgarwal/falcon
    daa3ffc [Pracheer Agarwal] FALCON-2225 extension owner added for trusted extensions
    622cae4 [Pracheer Agarwal] FALCON-2225 extension owner added for trusted extensions

--- webapp/src/main/java/org/apache/falcon/resource/ExtensionManager.java
+++ webapp/src/main/java/org/apache/falcon/resource/ExtensionManager.java
@@ -127,9 +127,9 @@ public class ExtensionManager extends AbstractExtensionManager {
             @DefaultValue("0") @QueryParam("offset") final Integer offset,
             @QueryParam("numResults") Integer resultsPerPage,
             @DefaultValue("") @QueryParam("doAs") String doAsUser) {
-        LOG.error("instances is not supported on Server.Please run your operation on Prism ");
-        throw FalconWebException.newAPIException("instances is not supported on Server. Please run your "
-                + "operation on Prism.");
+        LOG.error("instances is not supported on Falcon extensions. Use Falcon instance api on individual entities.");
+        throw FalconWebException.newAPIException("instances is not supported on Falcon extensions. Use Falcon instance "
+                + "api on individual entities.");
     }
 
     @GET
