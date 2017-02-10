bf027e16995a0487296517fc0024831733e4002a
sandeep <sandysmdl@gmail.com>
2017-02-10 08:41:40 +0530

    FALCON-2276 Falcon Trusted extensions submission and validation was failing
    Author: sandeep <sandysmdl@gmail.com>
    
    Reviewers: @pallavi-rao
    
    Closes #360 from sandeepSamudrala/FALCON-2276 and squashes the following commits:
    
    84169d6 [sandeep] FALCON-2276 Falcon Trusted extensions submission and validation was failing
    a3bd0e9 [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    db425c5 [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    3f67fed [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    cb2b00d [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    79e8d64 [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    7de7798 [sandeep] go -b FALCON-2263Merge branch 'master' of https://github.com/apache/falcon
    c5da0a2 [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    7e16263 [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    a234d94 [sandeep] FALCON-2231 Incoporated review comments and small fixes for duplicate submission and colo addition to schedule command
    26e3350 [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    73fbf75 [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    cc28658 [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    089b10d [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    456d4ee [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    0cf9af6 [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    4a2e23e [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    b1546ed [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    0a433fb [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    194f36a [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    e0ad358 [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    f96a084 [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    9cf36e9 [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    bbca081 [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    48f6afa [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    250cc46 [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    d0393e9 [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    a178805 [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    d6dc8bf [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    1bb8d3c [sandeep] Merge branch 'master' of https://github.com/apache/falcon
    c065566 [sandeep] reverting last line changes made
    1a4dcd2 [sandeep] rebased and resolved the conflicts from master
    271318b [sandeep] FALCON-2097. Adding UT to the new method for getting next instance time with Delay.
    a94d4fe [sandeep] rebasing from master
    9e68a57 [sandeep] FALCON-298. Feed update with replication delay creates holes

--- prism/src/main/java/org/apache/falcon/resource/proxy/ExtensionManagerProxy.java
+++ prism/src/main/java/org/apache/falcon/resource/proxy/ExtensionManagerProxy.java
@@ -28,7 +28,6 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
@@ -58,7 +57,6 @@ import org.apache.falcon.entity.v0.EntityType;
 import org.apache.falcon.entity.v0.feed.Feed;
 import org.apache.falcon.entity.v0.process.Process;
 import org.apache.falcon.extensions.Extension;
-import org.apache.falcon.extensions.ExtensionProperties;
 import org.apache.falcon.extensions.ExtensionService;
 import org.apache.falcon.extensions.ExtensionType;
 import org.apache.falcon.extensions.jdbc.ExtensionMetaStore;
@@ -300,7 +298,7 @@ public class ExtensionManagerProxy extends AbstractExtensionManager {
         List<Entity> entities;
         TreeMap<EntityType, List<Entity>> entityMap = new TreeMap<>();
         if (ExtensionType.TRUSTED.equals(extensionType)) {
-            entities = generateEntities(extensionName, config);
+            entities = extension.getEntities(extensionName, config);
             List<Entity> trustedFeeds = new ArrayList<>();
             List<Entity> trustedProcesses = new ArrayList<>();
             for (Entity entity : entities) {
@@ -568,7 +566,7 @@ public class ExtensionManagerProxy extends AbstractExtensionManager {
             throw FalconWebException.newAPIException("Extension validation is supported only for trusted extensions");
         }
         try {
-            List<Entity> entities = generateEntities(extensionName, request.getInputStream());
+            List<Entity> entities = extension.getEntities(extensionName, request.getInputStream());
             for (Entity entity : entities) {
                 super.validate(entity);
             }
@@ -717,19 +715,6 @@ public class ExtensionManagerProxy extends AbstractExtensionManager {
         } catch (Throwable e) {
             throw FalconWebException.newAPIException(e, Response.Status.INTERNAL_SERVER_ERROR);
         }
-    }
-
-    private List<Entity> generateEntities(String extensionName, InputStream configStream)
-        throws FalconException, IOException {
-        // get entities for extension job
-        Properties properties = new Properties();
-        properties.load(configStream);
-        List<Entity> entities = extension.getEntities(extensionName, configStream);
-
-        // add tags on extension name and job
-        String jobName = properties.getProperty(ExtensionProperties.JOB_NAME.getName());
-        EntityUtil.applyTags(extensionName, jobName, entities);
-        return entities;
     }
 
     private static void checkIfExtensionServiceIsEnabled() {
