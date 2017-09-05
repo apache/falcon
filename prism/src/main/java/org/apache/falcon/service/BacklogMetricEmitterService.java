c917fb47b2c6b2940c68729c85b558e13f51e550
Praveen Adlakha <praveen.adlakha@IM1314-L0.corp.inmobi.com>
2017-09-05 14:28:34 +0530

    comments addressed

--- prism/src/main/java/org/apache/falcon/service/BacklogMetricEmitterService.java
+++ prism/src/main/java/org/apache/falcon/service/BacklogMetricEmitterService.java
@@ -418,7 +418,7 @@ public final class BacklogMetricEmitterService implements FalconService,
                                     }
                                     InstancesResult status = wfEngine.getStatus(entity, nominalTime,
                                             new Date(nominalTime.getTime() + 200), PROCESS_LIFE_CYCLE, false);
-                                    if (status.getInstances().length > 0
+                                    if ( status !=null && status.getInstances().length > 0
                                             && status.getInstances()[0].status == InstancesResult.
                                             WorkflowStatus.SUCCEEDED) {
                                         LOG.debug("Instance of nominal time {} of entity {} has succeeded, removing "
